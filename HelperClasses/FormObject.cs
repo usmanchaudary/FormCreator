3e6563d8da3448bd1df93381b04797a13d1207d5
CH Usman <chusman2229@gmail.com>
2023-01-23 18:18:03 +0500

    Add project files.

---
+++ HelperClasses/FormObject.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace FormCreator.HelperClasses
+{
+    public class FormObject
+    {
+        /// <summary>
+        /// if the input is required or not
+        /// </summary>
+        public bool IsRequired { get; set; }
+        /// <summary>
+        /// if the input type is file or not
+        /// </summary>
+        public bool IsFile { get; set; }
+        /// <summary>
+        /// label to show on the input tag
+        /// </summary>
+        public string LabelName { get; set; }
+        /// <summary>
+        /// placeholder for the input types
+        /// </summary>
+        public string Placeholder { get; set; }
+        /// <summary>
+        /// the type of value which the user would enter in this field
+        /// </summary>
+        public string ValueType { get; set; }
+        /// <summary>
+        /// can be Text or Number
+        /// </summary>
+        public string ElementType { get; set; }
+        /// <summary>
+        /// in case of element type if drop down provide the list of options here.
+        /// </summary>
+        public string Options { get; set; } = string.Empty;
+        public List<string> GetOptions
+        {
+            get
+            {
+                return JsonConvert.DeserializeObject<List<string>>(value: Options);
+            }
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FormCreator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
